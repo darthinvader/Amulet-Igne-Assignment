7dbee719a159ef9b3dc54f5337504a9fe87fc4fd
darthinvader <psaradakisj@gmail.com>
2021-09-10 16:46:41 +0300

    [feat] working modal

--- src/components/LicensePlate/LicensePlate.js
+++ src/components/LicensePlate/LicensePlate.js
@@ -4,14 +4,28 @@ import image from "./car1.jpg";
 import styles from "./LicensePlate.module.scss";
 import LicensePlateInfo from "./LicensePlateInfo/LicensePlateInfo";
 import CarShowcase from "./CarShowcase/CarShowcase";
-
+import ErrorModal from "../ErrorModal/ErrorModal";
 const LicensePlate = () => {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(false);
+  const onError = () => {
+    setError(true);
+  };
+
+  const errorModal = error ? (
+    <ErrorModal
+      title="License Plate Does Not Exist"
+      onClose={() => {
+        setError(false);
+      }}
+    />
+  ) : null;
 
   return (
     <>
+      {errorModal}
       <img className={styles.Image} src={image} alt="Old Car" />
-      <LicensePlateInput setData={setData} />
+      <LicensePlateInput setData={setData} onError={onError} />
       {data ? (
         <>
           <LicensePlateInfo data={data} /> <CarShowcase />
--- src/components/LicensePlate/LicensePlateInput/LicensePlateInput.js
+++ src/components/LicensePlate/LicensePlateInput/LicensePlateInput.js
@@ -2,15 +2,21 @@ import axios from "axios";
 import { useState } from "react";
 import styles from "./LicensePlateInput.module.scss";
 
-const LicensePlateInput = ({ setData }) => {
+const LicensePlateInput = ({ setData, onError }) => {
   const [plateNum, setPlateNum] = useState("6-XXH-68");
 
   const requestData = async () => {
-    const request = await axios.get(
-      `https://api.overheid.io/voertuiggegevens/${plateNum}?ovio-api-key=${process.env.REACT_APP_API_KEY}`
-    );
-    setData(request.data);
-    console.log(request.data);
+    try {
+      const request = await axios.get(
+        `https://api.overheid.io/voertuiggegevens/${plateNum}?ovio-api-key=${process.env.REACT_APP_API_KEY}`
+      );
+
+      setData(request.data);
+      console.log(request.data);
+    } catch (err) {
+      console.log(err);
+      onError();
+    }
   };
   return (
     <div className={styles.Container}>
