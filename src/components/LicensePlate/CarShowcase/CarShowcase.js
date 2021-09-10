4136db5b50198bf15cf3299241e9e807a57ca3c9
darthinvader <psaradakisj@gmail.com>
2021-09-10 16:08:17 +0300

    [feat] working car showcase

--- src/components/LicensePlate/CarShowcase/CarShowcase.js
+++ src/components/LicensePlate/CarShowcase/CarShowcase.js
@@ -4,19 +4,40 @@ import car3 from "./car3.jpg";
 import { FcNext, FcPrevious } from "react-icons/fc";
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import styles from "./CarShowcase.module.scss";
+import { useState } from "react";
 const CarShowcase = () => {
+  const [cars, setCars] = useState([car1, car2, car3]);
+
   const images = (
     <div className={styles.ImagesContainer}>
-      <img className={styles.Image} src={car1} alt="Car 1" />
-      <img className={styles.Image} src={car2} alt="Car 2" />
-      <img className={styles.Image} src={car3} alt="Car 3" />
+      {cars.map((car, index) => (
+        <img
+          className={styles.Image}
+          key={index}
+          src={car}
+          alt={`Car ${index}`}
+        />
+      ))}
     </div>
   );
+  const SlideForward = () => {
+    let newCars = [...cars];
+    const firstElement = newCars.pop();
+    newCars = [firstElement, ...newCars];
+    setCars(newCars);
+  };
+  const SlideBack = () => {
+    let newCars = [...cars];
+    const lastElement = newCars.shift();
+    newCars = [...newCars, lastElement];
+    setCars(newCars);
+  };
+
   return (
     <div className={styles.Container}>
-      <GrFormPrevious className={styles.Arrow} />
+      <GrFormPrevious className={styles.Arrow} onClick={SlideBack} />
       {images}
-      <GrFormNext className={styles.Arrow} />
+      <GrFormNext className={styles.Arrow} onClick={SlideForward} />
     </div>
   );
 };
